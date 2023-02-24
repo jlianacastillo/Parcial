ba25e04dca91b510031309ab9523fbf8c8777e0b
jlianacastillo <lmov261220112@merida.tecnm.mx>
2023-02-23 18:42:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/parcial/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.parcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void lanzarBuenosDias(View  view){
+        Intent i = new Intent(this, BuenosDias.class);
+        startActivity(i);
+    }
+
+}
